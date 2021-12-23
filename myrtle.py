bf5328502e6ae3a33858f0f32ae7d3d3dcb1a4e3
mevouc <meven.courouble@vouc.me>
2021-12-23 15:37:31 +0000

    Disable spamming of "Bonjour <joiner>"

--- myrtle.py
+++ myrtle.py
@@ -66,8 +66,9 @@ class Myrtle(ircbot.SingleServerIRCBot):
 
     def on_join(self, serv, ev):
         joiner = irclib.nm_to_n(ev.source())
-        if (joiner != serv.get_nickname()):
-            serv.privmsg(ev.target(), "Bonjour " + joiner)
+        #if (joiner != serv.get_nickname()):
+        # hl useless because of splits
+        #    serv.privmsg(ev.target(), "Bonjour " + joiner)
 
     def on_part(self, serv, ev):
         parter = irclib.nm_to_n(ev.source())
